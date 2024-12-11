170193bd8f85b67534d0c2ec587f5792a4f7dfdd
QuickScythe <QuickScythe@gmail.com>
2024-12-10 18:52:37 -0600

    Remove resourcepack test

--- src/main/java/me/quickscythe/dragonforge/listeners/GuiListener.java
+++ src/main/java/me/quickscythe/dragonforge/listeners/GuiListener.java
@@ -28,27 +28,6 @@ public class GuiListener implements Listener {
 
     @EventHandler
     public void onPlayerCloseInventory(InventoryCloseEvent e) {
-        e.getPlayer().sendResourcePacks(() -> ResourcePackRequest.resourcePackRequest().packs(new ResourcePackInfoLike() {
-            @Override
-            public @NotNull ResourcePackInfo asResourcePackInfo() {
-                return new ResourcePackInfo() {
-                    @Override
-                    public @NotNull UUID id() {
-                        return null;
-                    }
-
-                    @Override
-                    public @NotNull URI uri() {
-                        return URI.create("https://ci.vanillaflux.com/view/DragonForge%20Creations/job/df_ll4el_resources/lastSuccessfulBuild/artifact/resources.zip");
-                    }
-
-                    @Override
-                    public @NotNull String hash() {
-                        return "" + new Date().getTime();
-                    }
-                };
-            }
-        }).replace(false).build());
         if (!e.getPlayer().hasMetadata("switchinv")) {
             GuiManager.closeGui((Player) e.getPlayer());
         }
